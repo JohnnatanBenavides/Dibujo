309141b0b158ac5e0df993b601f1c8aa53c3c0f5
JohnnatanBenavides <106650157+JohnnatanBenavides@users.noreply.github.com>
2022-06-28 11:02:34 -0500

    primera versión

---
+++ codigo.js
@@ -0,0 +1,38 @@
+let dibujo = document.getElementById("canvas");
+let  lienzo = dibujo.getContext("2d");
+
+function dibujarLinea(color, xInicial, yInicial, xFinal, yFinal)
+{
+    lienzo.beginPath();
+    lienzo.strokeStyle = color;
+    lienzo.moveTo(xInicial, yInicial);
+    lienzo.lineTo(xFinal, yFinal);
+    lienzo.stroke();
+    lienzo.closePath();
+}
+
+function dibujarArco(color, xCentro, yCentro, radio, angInicial, angFinal)
+{
+    lienzo.beginPath();
+    lienzo.strokeStyle = color;
+    lienzo.arc(xCentro, yCentro, radio, angInicial, angFinal);
+    lienzo.stroke();
+    lienzo.closePath();
+}
+
+dibujarLinea("black",250,150,250,280);
+dibujarLinea("black",200,200,300,200);
+dibujarLinea("black",200,200,180,210);
+dibujarLinea("black",300,200,320,190);
+dibujarLinea("black",250,280,200,310);
+dibujarLinea("black",250,280,300,310);
+dibujarLinea("black",200,310,200,320);
+dibujarLinea("black",300,310,300,320);
+
+dibujarArco("black",250,110,40,1,90);
+
+
+
+    
+
+    
